5c5382bd65c6e513dfb6c2c1008f3dc05b54b1c5
abellopez26 <abellopez2326@gmail.com>
2022-09-12 00:19:34 +0000

    feat: add adminlte

---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+
+
+
+Route::get('', [AdminController::class, "index"])->name('admin');
